7f9865706f4b4338c99d1a89016766add054d5f7
ronpeschel <138502235+ronpeschel@users.noreply.github.com>
2023-07-03 16:41:27 +0200

    Add files via upload

---
+++ HelloWorld.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function HelloWorld({age, name}) {
+    const headline = "Big Titles Are Awesome.";
+
+  return (
+    <div>
+        <div>
+        
+        <h1>Hello, {name}!
+            </h1>   
+           today you are {age} years old
+        </div>
+
+     
+            
+        
+         
+         
+    </div>
+   
+  )
+}
+
+export default HelloWorld
